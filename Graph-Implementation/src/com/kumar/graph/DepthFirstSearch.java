cc800ceb9ad2c7274b4539d845be663fc6f31584
unknown <rohandhakad20@gmail.com>
2023-08-19 23:28:52 +0530

    Depth first search implementation

--- Graph-Implementation/src/com/kumar/graph/DepthFirstSearch.java
+++ Graph-Implementation/src/com/kumar/graph/DepthFirstSearch.java
@@ -1,16 +1,15 @@
 package com.kumar.graph;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
-// BFS--> Level order traversal
-public class BreadthFirstSearch {
+public class DepthFirstSearch {
 	private LinkedList<Integer>[] adj;
 	private int vertices;
 	private int edges;
 	
-	public BreadthFirstSearch(int nodes) {
+	public DepthFirstSearch(int nodes) {
 		this.vertices = nodes;
 		this.edges = 0;
 		this.adj = new LinkedList[nodes];
@@ -25,27 +24,30 @@ public class BreadthFirstSearch {
 		this.edges++;
 	}
 	
-	public void BFS(int source, int nodes) {
+	public void DFS(int source, int nodes) {
 		boolean[] visited = new boolean[nodes+1];
-		Queue<Integer> q = new LinkedList<>();
-		visited[source] = true;
-		q.offer(source);
-		
-		while(!q.isEmpty()) {
-			int u = q.poll();
-			System.out.print(u + " ");
-			for(int v:adj[u]) {
-				if(visited[v] == false) {
-					visited[v] = true;
-					q.offer(v);
+		Stack<Integer> stack = new Stack<>();
+		stack.push(source);
+		while(!stack.isEmpty()) {
+			int u = stack.pop();
+			if(visited[u] == false) {
+				visited[u] = true;
+				System.out.print(u+", ");
+				
+				for(int v: adj[u]) {
+					if(visited[v] == false) {
+						stack.push(v);
+					}
 				}
 			}
 		}
+		
+		
 	}
 	
 	public static void main(String[] args) {
 		int nodes = 5;
-		BreadthFirstSearch g = new BreadthFirstSearch(nodes);
+		DepthFirstSearch g = new DepthFirstSearch(nodes);
 		g.addEdge(0, 1);
 		g.addEdge(0, 3);
 		g.addEdge(1, 2);
@@ -58,9 +60,8 @@ public class BreadthFirstSearch {
 		}
 		System.out.println("Totla number of edges: "+g.edges);
 		System.out.print("\n");
-		System.out.println("Breadth first search-> ");
-		g.BFS(0, nodes);
+		System.out.println("Depth first search-> ");
+		g.DFS(0, nodes);
 
 	}
-
 }
