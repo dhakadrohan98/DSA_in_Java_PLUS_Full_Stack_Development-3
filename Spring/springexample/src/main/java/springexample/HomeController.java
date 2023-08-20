a005cb6be141b442f92d0aced0bcba878f74d278
unknown <rohandhakad20@gmail.com>
2023-08-20 13:58:47 +0530

    Recursive DFS call

--- Spring/springexample/src/main/java/springexample/HomeController.java
+++ Spring/springexample/src/main/java/springexample/HomeController.java
@@ -1,7 +1,9 @@
 package springexample;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -10,5 +12,13 @@ public class HomeController {
 	public String home() {
 		System.out.println("Landing to home page!!!");
 		return "home";
+	}
+	
+	//handling for request
+	@RequestMapping("/welcome")
+	public String welcome(@RequestParam("user") String name,Model m) {
+		System.out.println("User Name: "+name);
+		m.addAttribute("name", name);
+		return "welcome";
 	}
 }
