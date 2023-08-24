7c8c83e529c5c2cbe4c424a0d6bab73e465679f4
unknown <rohandhakad20@gmail.com>
2023-08-24 16:39:28 +0530

    Iterative InOrder traversal methods are written

--- Binary-Tree/src/com/kumar/binarytree/TreeTraversalMethods.java
+++ Binary-Tree/src/com/kumar/binarytree/TreeTraversalMethods.java
@@ -5,11 +5,11 @@ public class TreeTraversalMethods {
 	public static void main(String[] args) {
 		TreeNode tn = new TreeNode();
 		tn.createBinaryTree();
-		System.out.print("PreOrder traversal: ");
+		System.out.print(" Iterative PreOrder traversal: ");
 		tn.itrPreorder(tn.getRoot());
 		System.out.println();
-		System.out.print("Inorder traversal: ");
-		tn.recInorder(tn.getRoot());
+		System.out.print("Iterative Inorder traversal: ");
+		tn.itrInorder(tn.getRoot());
 
 	}
 
