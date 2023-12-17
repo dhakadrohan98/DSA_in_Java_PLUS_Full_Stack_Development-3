1a4b107fd9a61ac18ea9f34b819bf51a3e1f12be
unknown <dhakadrohan98>
2023-12-17 23:37:38 +0530

    singleton pattern is implemented

---
+++ Comdo/src/com/comdo/comdo.java
@@ -0,0 +1,10 @@
+package com.comdo;
+
+public class comdo {
+
+	public static void main(String[] args) {
+		System.out.println("Hi comdo");
+
+	}
+
+}
---
+++ Design_Patterns/src/org/codeship/Main.java
@@ -0,0 +1,15 @@
+package org.codeship;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Database db=null;
+		
+		Database object = db.getInstance();
+		System.out.println(object);
+		
+		Database object1 = db.getInstance();
+		System.out.println(object1);
+		
+	}
+}
