02dd81b95026ea865d35357f17efe6cc0578e646
unknown <dhakadrohan98>
2023-12-23 13:52:06 +0530

    tested methods of list, set & map interface in collections framework and implments runnable, extended Thread class in multithreading

---
+++ ArrayList/src/com/codeship/GenericType.java
@@ -0,0 +1,24 @@
+package com.codeship;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class GenericType {
+
+	public static void main(String[] args) {
+		ArrayList templist = new ArrayList();
+		templist.add(1);
+		templist.add("Rohan");
+		templist.add(62353.65);
+		
+//		Iterator it = templist.iterator();
+//		
+//		if(it.hasNext()) {
+//			System.out.println(it.next());
+//		}
+		System.out.println("List: "+templist);
+
+	}
+
+}
