2f2925e8b6a32b4d188e37d673c468f0b2d3ee51
unknown <dhakadrohan98>
2024-01-03 13:29:10 +0530

    ways of creation Stream API & its methods are tested

--- Interview_Practice/src/org/stream/api/StreamMain.java
+++ Interview_Practice/src/org/stream/api/StreamMain.java
@@ -11,9 +11,11 @@ public class StreamMain {
 		List<Integer> list1 = List.of(2,4,50,21,22,67,99,98,97,96,78,75,77,76);
 //		Stream<Integer> stream = list1.stream();
 		List<Integer> collect1 = list1.stream().filter(n -> n % 2 != 0).collect(Collectors.toList());
+		System.out.println("odd numbers:");
 		System.out.println(collect1);
 		
-		List<Integer> collect2 = list1.stream().filter(n -> {return n>50;}).collect(Collectors.toList());
+		System.out.println("numbers greater than 50:");
+		List<Integer> collect2 = list1.stream().filter(n -> n>50).collect(Collectors.toList());
 		System.out.println(collect2);
 		
 		
