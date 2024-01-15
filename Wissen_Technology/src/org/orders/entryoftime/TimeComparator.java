2e0a6c17b1cdf0021dd355d7c48a00c321387831
unknown <dhakadrohan98>
2024-01-15 13:46:55 +0530

    Train Ticket Entry Of Time

--- Wissen_Technology/src/org/orders/entryoftime/TimeComparator.java
+++ Wissen_Technology/src/org/orders/entryoftime/TimeComparator.java
@@ -7,18 +7,6 @@ public class TimeComparator implements Comparator<Order> {
 	@Override
 	public int compare(Order o1, Order o2) {
 		
-		int hour1 = o1.timeOfEntry.getHour();
-		int minute1 =  o1.timeOfEntry.getMinute();
-		
-		int hour2 = o1.timeOfEntry.getHour();
-		int minute2 =  o1.timeOfEntry.getMinute();
-		
-		if(hour1 == hour2 && minute1 == minute2) {
-			return 0;
-		} else if(hour1 >= hour2 && minute1 >= minute2){
-			return 1;
-		} else {
-			return -1;
-		}
+		return o1.getTimeOfEntry().compareTo(o2.getTimeOfEntry());
 	}
 }
