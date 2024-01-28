8457df82fe25e9361f42114a098117600eb70d6c
unknown <dhakadrohan98>
2024-01-29 00:21:29 +0530

    Leetcode Rearrange Array Elements by Sign problem solved

---
+++ Array/src/org/leetcodemedium/arrays/RearrangeArrayElementsbySign1.java
@@ -0,0 +1,29 @@
+package org.leetcodemedium.arrays;
+
+public class RearrangeArrayElementsbySign1 {
+
+	 public int[] rearrangeArray(int[] nums) {
+	        int[] result = new int[nums.length];
+	        int positiveIndex = 0;
+	        int negativeIndex = 1;
+	        
+	        for(int i=0; i<nums.length; i++) {
+	            
+	            if(nums[i] > 0) {
+	                result[positiveIndex] = nums[i];
+	                positiveIndex += 2;
+	            }
+	            else {
+	                result[negativeIndex] = nums[i];
+	                negativeIndex += 2;
+	            }
+	        }
+	        return result;
+	 }
+	 
+	public static void main(String[] args) {
+		
+
+	}
+
+}
