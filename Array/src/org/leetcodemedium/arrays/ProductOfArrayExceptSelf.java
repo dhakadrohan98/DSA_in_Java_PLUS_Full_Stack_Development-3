0a230e1c5a6d8aef0dc3c88b5bd245ae5794a822
unknown <dhakadrohan98>
2024-01-29 23:49:25 +0530

    Leetcode Longest consecutive subsequence problem solved

---
+++ Array/src/org/leetcodemedium/arrays/ProductOfArrayExceptSelf.java
@@ -0,0 +1,28 @@
+package org.leetcodemedium.arrays;
+
+public class ProductOfArrayExceptSelf {
+
+	public int[] productExceptSelf(int[] nums) {
+        int[] result = new int[nums.length];
+        
+        int temp = 1;
+        for(int i=0; i<nums.length; i++) {
+            result[i] = temp;
+            temp = temp * nums[i];
+        }
+        
+        temp=1;
+        for(int i=nums.length-1; i>=0; i--) {
+            result[i] = result[i] * temp;
+            temp = temp * nums[i];
+        }
+        
+        return result;
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
