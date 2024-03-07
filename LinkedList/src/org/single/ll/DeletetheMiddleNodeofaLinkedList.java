af7bca3a6d741fc05535bef3183b55b9ac202ab4
unknown <dhakadrohan98>
2024-03-07 21:05:06 +0530

    Optimised solution for Delete the middle node of SLL problem

--- LinkedList/src/org/single/ll/DeletetheMiddleNodeofaLinkedList.java
+++ LinkedList/src/org/single/ll/DeletetheMiddleNodeofaLinkedList.java
@@ -2,7 +2,26 @@ package org.single.ll;
 
 public class DeletetheMiddleNodeofaLinkedList {
 
-	
+	//TC: O(n/2)
+	//SC: O(1)
+	public ListNode deleteMiddle(ListNode head) {
+        //base case
+        if(head == null || head.next == null) {
+            return null;
+        }
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        fast = fast.next.next;
+        
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;   
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+	 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
