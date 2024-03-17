c5f818f3c50511a9eca0eb781c5492ac23fbdde1
unknown <dhakadrohan98>
2024-03-17 19:09:40 +0530

    Palindrome linked list problem is solved

---
+++ LinkedList/src/org/single/ll/IntersectionofTwoLinkedLists1.java
@@ -0,0 +1,14 @@
+package org.single.ll;
+
+public class IntersectionofTwoLinkedLists1 {
+
+	public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
