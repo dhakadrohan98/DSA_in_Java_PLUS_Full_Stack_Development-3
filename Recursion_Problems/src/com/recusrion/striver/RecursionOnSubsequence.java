8a54e5bd665345fad29f59d78007589e07a35dcd
unknown <dhakadrohan98>
2024-03-25 17:01:44 +0530

    Subsquence using recursion problem is solved

---
+++ Recursion_Problems/src/com/recusrion/striver/RecursionOnSubsequence.java
@@ -0,0 +1,38 @@
+package com.recusrion.striver;
+
+import java.util.*;
+
+public class RecursionOnSubsequence {
+
+	private static void print(int indx, List<Integer> list, int[] arr, int n) {
+		// base case
+		if (indx == n) {
+			for (int temp : list) {
+				System.out.print(temp + " ");
+			}
+			if (list.size() != 0) {
+				System.out.println();
+			}
+			// empty set
+			if (list.size() == 0) {
+				System.out.println("{}");
+			}
+			return;
+		}
+		//take or pick the particular index into the subsequence
+		list.add(arr[indx]);
+		print(indx + 1, list, arr, n);
+		list.remove(Integer.valueOf(arr[indx])); //remove(Object o) from the list or a particular element
+		// not pick or not take condition, this element is not added to your subsequence
+		print(indx + 1, list, arr, n);
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 3, 1, 2 };
+		List<Integer> list = new ArrayList<>();
+		int n = arr.length;
+		print(0, list, arr, n);
+
+	}
+
+}
