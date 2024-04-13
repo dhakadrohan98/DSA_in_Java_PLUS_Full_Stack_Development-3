1156b8e051dde70bf0db63afe1a034a529d690fc
unknown <dhakadrohan98>
2024-04-14 01:58:06 +0530

    Path SUM-III problem is solved. TC: O(n^2)

---
+++ Tree/src/org/binary/tree/PathSumIII.java
@@ -0,0 +1,37 @@
+package org.binary.tree;
+
+public class PathSumIII {
+
+	int total = 0;
+    public int pathSum(TreeNode root, int targetSum) {
+        //edge case
+        if(root == null) return 0;
+        
+        //another methods call
+        findPathSum(root, targetSum, 0);
+        //recursive call on left & right subtree
+        pathSum(root.left, targetSum);
+        pathSum(root.right, targetSum);
+        return total;
+    }
+    
+    public void findPathSum(TreeNode root, int targetSum, int currSum) {
+        //base case
+        if(root == null) return;
+        
+        currSum += root.val;
+        
+        if(targetSum == currSum)
+            total++;
+        
+        findPathSum(root.left, targetSum, currSum);
+        findPathSum(root.right, targetSum, currSum);
+        
+    }
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
