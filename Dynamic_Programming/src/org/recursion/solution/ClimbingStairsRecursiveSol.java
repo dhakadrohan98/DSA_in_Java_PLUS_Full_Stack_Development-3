24d1147d135d1fdfbd2690efa66ced96e5e76a0b
unknown <dhakadrohan98>
2024-04-18 02:04:32 +0530

    Flipkart online ass. problem is solved TC: O(n^2) and SC: O(n)

--- Dynamic_Programming/src/org/recursion/solution/ClimbingStairsRecursiveSol.java
+++ Dynamic_Programming/src/org/recursion/solution/ClimbingStairsRecursiveSol.java
@@ -1,13 +1,16 @@
 package org.recursion.solution;
 
 public class ClimbingStairsRecursiveSol {
-
+	//TC: O(n)
+	//SC: O(n) - stack space
+	//Recursive solution
 	public static int climbStairs(int n) {
 		//two base case
 		if(n == 0 || n == 1) return 1;
 		
 		int left = climbStairs(n-1);
 		int right = climbStairs(n-2);
+		//sum of all stuffs
 		return left + right;
 	}
 	public static void main(String[] args) {
