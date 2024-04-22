86f5f43639d3234146c6e05977ff4a5fe1372c38
unknown <dhakadrohan98>
2024-04-22 15:01:28 +0530

    Kth smallest element problem is solved. TC: O(N) and SC: O(1)

--- Tree_Lucky_Kumar/src/org/binary/search/tree/KthSmallestElement.java
+++ Tree_Lucky_Kumar/src/org/binary/search/tree/KthSmallestElement.java
@@ -2,23 +2,48 @@ package org.binary.search.tree;
 
 import java.util.*;
 
+//Optimised solution
 public class KthSmallestElement {
-	//TC: O(N)
-	//SC: O(N)stack space + O(N)ArrayList
+	// TC: O(N)
+	// SC: O(1)
+	int count = 0;
+
 	public int kthSmallest(TreeNode root, int k) {
 		List<Integer> list = new ArrayList<>();
-		dfs(root, list);
-		int smallest = list.get(k-1);
-		return smallest;
+		dfs(root, list, k);
+		return list.get(0);
 	}
 
-	//inorder traversal to store elements in sorted fashion
-	private void dfs(TreeNode root, List<Integer> list) {
-		if(root == null) return;
-		
-		dfs(root.left, list);
-		list.add(root.val);
-		dfs(root.right, list);
-		
+	private void dfs(TreeNode root, List<Integer> list, int k) {
+		// two base case
+		if (root == null)
+			return;
+
+		dfs(root.left, list, k);
+		count++; // cnt = 1
+		if (k == count) {
+			list.add(root.val);
+		}
+		dfs(root.right, list, k);
 	}
 }
+
+////TC: O(N)
+//	// SC: O(N)stack space + O(N)ArrayList
+//	public int kthSmallest(TreeNode root, int k) {
+//		List<Integer> list = new ArrayList<>();
+//		dfs(root, list);
+//		int smallest = list.get(k - 1);
+//		return smallest;
+//	}
+//
+//	// inorder traversal to store elements in sorted fashion
+//	private void dfs(TreeNode root, List<Integer> list) {
+//		if (root == null)
+//			return;
+//
+//		dfs(root.left, list);
+//		list.add(root.val);
+//		dfs(root.right, list);
+//
+//	}
--- Tree_Lucky_Kumar/src/org/binary/search/tree/RangeSumOfBST.java
+++ Tree_Lucky_Kumar/src/org/binary/search/tree/RangeSumOfBST.java
@@ -1,5 +1,51 @@
 package org.binary.search.tree;
 
 public class RangeSumOfBST {
+	//TC: < O(N) in average cases & O(N) in worst cases
+	//SC: < O(N) in average cases & O(N) in worst cases
+	int sum = 0;
 
+	public int rangeSumBST(TreeNode root, int low, int high) {
+		dfs(root, low, high);
+		return sum;
+	}
+
+	public void dfs(TreeNode root, int low, int high) {
+		// base case
+		if (root == null)
+			return;
+
+		if (low <= root.val && root.val <= high) {
+			sum += root.val;
+		}
+		
+		 // Corrected order of recursive calls
+	    if (root.val > low) {
+	        dfs(root.left, low, high);
+	    }
+	    if (root.val < high) {
+	        dfs(root.right, low, high);
+	    }
+	}
 }
+
+//TC: O(N)
+//SC: O(N) stack space
+//int sum = 0;
+//
+//public int rangeSumBST(TreeNode root, int low, int high) {
+//	dfs(root, low, high);
+//	return sum;
+//}
+//
+//public void dfs(TreeNode root, int low, int high) {
+//	// base case
+//	if (root == null)
+//		return;
+//
+//	if (low <= root.val && root.val <= high) {
+//		sum += root.val;
+//	}
+//	dfs(root.left, low, high);
+//	dfs(root.right, low, high);
+//}
