23d409fac686d54977ae532518e5a999d6c2b137
unknown <dhakadrohan98>
2024-04-23 17:54:05 +0530

    Training Ninja's using problem is solved memoization technique. TC: O(N) and SC: O(N) stack space + O(N)

---
+++ Dynamic_Programming/src/org/recursion/dp/solution/NinjaTrainingDPRecursiveSol.java
@@ -0,0 +1,53 @@
+package org.recursion.dp.solution;
+
+import java.util.Arrays;
+
+public class NinjaTrainingDPRecursiveSol {
+	public static int ninjaTraining(int n, int points[][]) {
+		int[][] dp = new int[n][4];
+		for (int i = 0; i < n; i++) {
+			Arrays.fill(dp[i], -1);
+		}
+		return dfs(n - 1, 3, points, dp);
+	}
+
+	private static int dfs(int day, int last, int[][] points, int[][] dp) {
+		// Two base cases
+		if (dp[day][last] != -1) {
+			return dp[day][last];
+		}
+
+		if (day == 0) {
+			int maxi = 0;
+			for (int i = 0; i < 3; i++) {
+				if (i != last) {
+					maxi = Math.max(maxi, points[day][i]);
+				}
+			}
+			return dp[day][last] = maxi;
+		}
+
+		// caculating for n-1,n-2,......,1 indexes
+		int maxi = 0;
+		for (int i = 0; i < 3; i++) {
+			if (i != last) {
+				int maxPoints = points[day][i] + dfs(day - 1, i, points, dp);
+				maxi = Math.max(maxi, maxPoints);
+			}
+		}
+		return dp[day][last] = maxi;
+	}
+
+	public static void main(String[] args) {
+		int n = 4;
+		int[][] points = new int[n][];
+		points[0] = new int[] { 2, 1, 3 };
+		points[1] = new int[] { 3, 4, 6 };
+		points[2] = new int[] { 10, 1, 6 };
+		points[3] = new int[] { 8, 3, 7 };
+
+		int ans = ninjaTraining(n, points);
+		System.out.println();
+		System.out.println(ans);
+	}
+}
--- Dynamic_Programming/src/org/recursion/pure/solution/NinjaTrainingPureRecursiveSol.java
+++ Dynamic_Programming/src/org/recursion/pure/solution/NinjaTrainingPureRecursiveSol.java
@@ -1,6 +1,8 @@
 package org.recursion.pure.solution;
 
 public class NinjaTrainingPureRecursiveSol {
+	//TC: O(2^n)
+	//SC: O(N) stack space
 	public static int ninjaTraining(int n, int points[][]) {
 		return dfs(n - 1, 3, points);
 	}
@@ -18,12 +20,26 @@ public class NinjaTrainingPureRecursiveSol {
 		}
 		// caculating for n-1,n-2,......,1 indexes
 		int maxi = Integer.MIN_VALUE;
-		for (int i = 0; i < 3; i++) {
+		for (int i = 0; i < 3; i++) { // day = 1, i = 2; arr[1][2]
 			if (i != last) {
-				int maxPoints = points[day][i] + dfs(day-1, i, points);
+				int maxPoints = points[day][i] + dfs(day-1, i, points);  //day=0 z, i=0
+				System.out.println("Day: " + day + " i: " + i + " maxPoints: " + maxPoints);
 				maxi = Math.max(maxi, maxPoints);
 			}
 		}
 		return maxi;
+	}
+	
+	public static void main(String[] args) {
+		int n = 4;
+		int[][] points = new int[n][];
+		points[0] = new int[]{2,1,3};
+		points[1] = new int[]{3,4,6};
+		points[2] = new int[]{10,1,6};
+		points[3] = new int[]{8,3,7};
+		
+		int ans = ninjaTraining(n, points);
+		System.out.println();
+		System.out.println(ans);
 	}
 }
