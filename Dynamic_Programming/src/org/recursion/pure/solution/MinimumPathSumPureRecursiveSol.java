1a4632a753ed157558f7f2f43d0c0cd8e59b3c49
unknown <dhakadrohan98>
2024-04-25 00:04:35 +0530

    Minimum Path Sum problem is solved through recursion. TC: O(2 ^ (m * n)) and SC: O(m * n)

---
+++ Dynamic_Programming/src/org/recursion/pure/solution/MinimumPathSumPureRecursiveSol.java
@@ -0,0 +1,27 @@
+package org.recursion.pure.solution;
+
+public class MinimumPathSumPureRecursiveSol {
+	//TC: O(2 ^ (m * n))
+	//SC: O(m * n) stack space
+	public int minPathSum(int[][] grid) {
+		int m = grid.length;
+		int n = grid[0].length;
+		return rec(grid, m-1, n-1);
+	}
+
+	private int rec(int[][] grid, int i, int j) {
+		//base case
+		if(i == 0 && j == 0) {
+			return grid[0][0];
+		}
+		
+		if(i < 0 || j < 0) {
+			return (int) Math.pow(10, 9);
+		}
+		int up = grid[i][j] + rec(grid, i-1, j);
+		int down = grid[i][j] + rec(grid, i, j-1);
+		return Math.min(up, down);
+	}
+	
+	
+}
