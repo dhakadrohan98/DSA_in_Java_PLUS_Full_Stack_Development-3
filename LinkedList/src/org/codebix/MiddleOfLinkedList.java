4025acb13d2921b84b0b56677780ca13fc8e35e3
unknown <dhakadrohan98>
2024-04-29 16:32:21 +0530

    Right side view of BT problem is solved, TC: O(N) & SC: O(N)

---
+++ LinkedList/src/org/codebix/MiddleOfLinkedList.java
@@ -0,0 +1,15 @@
+package org.codebix;
+
+public class MiddleOfLinkedList {
+public ListNode middleNode(ListNode head) {
+        
+        ListNode fast = head;
+        ListNode slow = head;
+        
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
