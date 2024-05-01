5ec461163b6c864fad180d654cbed20cd05cb147
unknown <dhakadrohan98>
2024-05-01 16:33:59 +0530

    Longest consecutive subsequence problem is solved, TC: O(n) & SC: O(n)

--- Heap/src/org/heap/algorithms/KthLargestElementInArray.java
+++ Heap/src/org/heap/algorithms/KthLargestElementInArray.java
@@ -3,17 +3,17 @@ package org.heap.algorithms;
 import java.util.PriorityQueue;
 
 public class KthLargestElementInArray {
-	
+
 	public static int findKthLargest(int[] nums, int k) {
 		int n = nums.length;
 		PriorityQueue<Integer> pq = new PriorityQueue<>();
-		
-		for(int i = 0; i < k; i++) {
+
+		for (int i = 0; i < k; i++) {
 			pq.offer(nums[i]);
 		}
-		
-		for(int i = k; i < n; i++) {
-			if(nums[i] > pq.peek()) {
+
+		for (int i = k; i < n; i++) {
+			if (nums[i] > pq.peek()) {
 				pq.poll();
 				pq.offer(nums[i]);
 			}
@@ -22,8 +22,8 @@ public class KthLargestElementInArray {
 	}
 
 	public static void main(String[] args) {
-		int[] nums = {3,2,3,1,2,4,5,5,6};
-		int[] nums1 = {3,2,1,5,6,4};
+		int[] nums = { 3, 2, 3, 1, 2, 4, 5, 5, 6 };
+		int[] nums1 = { 3, 2, 1, 5, 6, 4 };
 		int k = 4;
 		int k1 = 2;
 		System.out.println(findKthLargest(nums1, k1));
---
+++ ParkPlusDSAProblems/src/org/dsa/problems/WaysToMakeCoinChange.java
@@ -0,0 +1,43 @@
+package org.dsa.problems;
+
+import java.util.*;
+
+public class WaysToMakeCoinChange {
+
+	public long count(int[] coins, int n, int target) {
+		long dp[][] = new long[n][target + 1];
+		for (long[] temp : dp) {
+			Arrays.fill(temp, -1);
+		}
+		return helper(coins, dp, n - 1, target);
+	}
+
+	private long helper(int[] coins, long[][] dp, int indx, int target) {
+		// base case
+		if (indx == 0) {
+			if (target % coins[indx] == 0) {
+				return 1;
+			} else {
+				return 0;
+			}
+		}
+
+		if (dp[indx][target] != -1)
+			return dp[indx][target];
+
+		long take = 0;
+		if (coins[indx] <= target) {
+			//keep indx constant to consider same element again & again.Reduce the target to stop 
+			//considering same element.
+			take = helper(coins, dp, indx, target - coins[indx]);
+		}
+		long notTake = helper(coins, dp, indx - 1, target);
+		return dp[indx][target] = take + notTake;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
