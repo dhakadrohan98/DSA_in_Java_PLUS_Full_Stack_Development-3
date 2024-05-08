c5fd6fd850c21f0acaa73ed13481645a0e00a1f7
unknown <dhakadrohan98>
2024-05-09 00:36:38 +0530

    closed island problem is solved, TC: O(m*n) & SC: O(m*n)

--- Graph/src/org/graph/lucky/bhaiyan/NumberOfClosedIsland.java
+++ Graph/src/org/graph/lucky/bhaiyan/NumberOfClosedIsland.java
@@ -1,9 +1,9 @@
 package org.graph.lucky.bhaiyan;
 
-public class SurroundedRegions {
-	//TC: O(m * n)
+public class NumberOfClosedIsland {
+	//TC: O(m * n) + O(m) + O(n)
 	//SC: O(m * n)
-	private void dfs(int row, int col, boolean[][] visited, char[][] mat, int[] delrow, int[] delcol) {
+	private void dfs(int row, int col, boolean[][] visited, int[][] mat, int[] delrow, int[] delcol) {
 		// mark visit to the current cell
 		visited[row][col] = true;
 		int n = mat.length;
@@ -14,29 +14,30 @@ public class SurroundedRegions {
 			int ncol = col + delcol[i];
 			// check for valid coordinates and unvisited O's
 			if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && visited[nrow][ncol] == false
-					&& mat[nrow][ncol] == 'O') {
+					&& mat[nrow][ncol] == 0) {
 				// recursive call on adjacent cells with value O
 				dfs(nrow, ncol, visited, mat, delrow, delcol);
 			}
 		}
 	}
-
-	public void solve(char[][] mat) {
-		int n = mat.length; // n rows
-		int m = mat[0].length; // m columns
-		int[] delrow = { -1, 0, +1, 0 }; // up right down left
+    
+    public int closedIsland(int[][] mat) {
+        int n = mat.length;
+		int m = mat[0].length;
+        int[] delrow = { -1, 0, +1, 0 }; // up right down left
 		int[] delcol = { 0, +1, 0, -1 }; // up right down left
 		boolean[][] visited = new boolean[n][m];
+        int closedIsland = 0;
 		// traverse first row and last row
 		for (int j = 0; j < m; j++) {
 			// check for unvisited Os in the boundary rows
 			// first row
-			if (visited[0][j] == false && mat[0][j] == 'O') {
+			if(visited[0][j] == false && mat[0][j] == 0) {
 				dfs(0, j, visited, mat, delrow, delcol);
 			}
 
 			// last row
-			if (visited[n - 1][j] == false && mat[n - 1][j] == 'O') {
+			if (visited[n - 1][j] == false && mat[n - 1][j] == 0) {
 				dfs(n - 1, j, visited, mat, delrow, delcol);
 			}
 		}
@@ -44,12 +45,12 @@ public class SurroundedRegions {
 		for (int i = 0; i < n; i++) {
 			// check for unvisited Os in the boundary columns
 			// first column
-			if (!visited[i][0] && mat[i][0] == 'O') {
+			if (!visited[i][0] && mat[i][0] == 0) {
 				dfs(i, 0, visited, mat, delrow, delcol);
 			}
 
 			// last column
-			if (!visited[i][m - 1] && mat[i][m - 1] == 'O') {
+			if (!visited[i][m - 1] && mat[i][m - 1] == 0) {
 				dfs(i, m - 1, visited, mat, delrow, delcol);
 			}
 		}
@@ -57,9 +58,12 @@ public class SurroundedRegions {
 		// if unvisited O then convert to X
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
-				if (visited[i][j] == false && mat[i][j] == 'O')
-					mat[i][j] = 'X';
+				if (visited[i][j] == false && mat[i][j] == 0) {
+                    dfs(i, j, visited, mat, delrow, delcol);
+                    closedIsland++;
+                }
 			}
 		}
-	}
+        return closedIsland;
+    }
 }
