84efe636e8d5999301ac8b94e7a853ebd1d71ec0
unknown <dhakadrohan98>
2024-05-09 01:47:58 +0530

    flood fill problem is solved, TC: O(v+e) & SC: O(v)

---
+++ Graph/src/org/graph/lucky/bhaiyan/FloodFill.java
@@ -0,0 +1,30 @@
+package org.graph.lucky.bhaiyan;
+
+public class FloodFill {
+	//TC: O(V + E)
+	//SC: O(V)
+	private void dfs(int[][] image, int[][] ans, int row, int col, int newColor, int iniColor) {
+		int m = image.length;
+		int n = image[0].length;
+		//base case
+		if(row < 0 || row >= m || col < 0 || col >= n || image[row][col] !=  iniColor || 
+				ans[row][col] == newColor) 
+		{
+			return;
+		}
+		ans[row][col] = newColor;
+		//recursive call
+		dfs(image, ans, row-1, col, newColor, iniColor);
+		dfs(image, ans, row, col+1, newColor, iniColor);
+		dfs(image, ans, row+1, col, newColor, iniColor);
+		dfs(image, ans, row, col-1, newColor, iniColor);
+	}
+	
+	public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+		int iniColor = image[sr][sc];
+		//making copy of the given array
+		int[][] ans = image;
+		dfs(image, ans, sr, sc, newColor, iniColor);
+		return ans;
+	}
+}
