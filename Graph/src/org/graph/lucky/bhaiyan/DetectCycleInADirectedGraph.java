bbd4ca7cc040b7d885ec94a11ed47e287d6dc25a
unknown <dhakadrohan98>
2024-05-13 15:22:56 +0530

    Detect cycle in a directed graph problems are solved. TC: O(E+V) and SC: O(2*V)

---
+++ Graph/src/org/graph/lucky/bhaiyan/DetectCycleInADirectedGraph.java
@@ -0,0 +1,40 @@
+package org.graph.lucky.bhaiyan;
+
+import java.util.*;
+
+public class DetectCycleInADirectedGraph {
+	//TC: O(v+e)
+	//SC: O(2*v)
+	public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
+        boolean[] visited = new boolean[V];
+        boolean[] helper = new boolean[V];
+        for(int i=0; i<V; i++) {
+        	if(visited[i] == false) {
+        		//pick first node of different components & then apply DFS on this node
+        		boolean ans = DFS(adj, i, visited, helper);
+        		//if cycle detected then return true from here
+        		if(ans == true) return true;
+        	}
+        }
+      //if cycle is not found then return false.
+        return false;
+    }
+
+	private boolean DFS(ArrayList<ArrayList<Integer>> adj, int i, boolean[] visited, boolean[] helper) {
+		visited[i] = true;
+		helper[i] = true;
+		ArrayList<Integer> neighbours = adj.get(i);//store all neighbour nodes into ArrayList
+		for(int k=0; k<neighbours.size(); k++) {
+			int curr = neighbours.get(k);
+			//if cycle detected then return true from here
+			if(helper[curr] == true) return true;
+			if(visited[curr] == false) {
+				boolean ans = DFS(adj, curr, visited, helper);
+				if(ans == true) return true;
+			}
+		}
+		//mark unvisit the current node while doing backtrack in DFS (DFS in reverse order through backtracking)
+		helper[i] = false;
+		return false;
+	}
+}
