f1d3e314e0413bc87f25404e19b46b4d413ade79
unknown <dhakadrohan98>
2024-05-23 00:55:47 +0530

    Sort character by freq, next greater element problems are solved

--- Wissen_Technology/src/com/PreDirectorRound/LargestOddNumber.java
+++ Wissen_Technology/src/com/PreDirectorRound/LargestOddNumber.java
@@ -3,14 +3,14 @@ package com.PreDirectorRound;
 public class LargestOddNumber {
 
 	public static String largestOddNumber(String num) {
-		
+
 		String res = "";
 		for (int i = num.length() - 1; i >= 0; i--) {
 			char ch = num.charAt(i);
 			int x = ch - '0'; // convert character into numeric value through ASCII code
 			if (x % 2 != 0) {
 				System.out.println(x);
-				res = num.substring(0, i+1);
+				res = num.substring(0, i + 1);
 				break;
 			}
 
