c833802cb12c26969581bc6e1cc8a4fcaaf24373
RohanDhakad <dhakadrohan98>
2025-03-22 18:20:52 +0530

    Both bugs is resolved in searchItem() method based on various parameters & sorting sequence

--- Flipkart_Inventory/src/org/codeship/Driver.java
+++ Flipkart_Inventory/src/org/codeship/Driver.java
@@ -9,40 +9,42 @@ import org.codeship.service.InventoryService;
 
 public class Driver {
 
-	public static void main(String[] args) {
-		InventoryService inventoryService = new InventoryService(new InventoryDao());
-		try {
-			// add items
-			inventoryService.addItem("Amul", "Milk", 100);
-			inventoryService.addItem("Amul", "Curd", 50);
-			inventoryService.addItem("Nestle", "Milk", 60);
-			inventoryService.addItem("Nestle", "Curd", 90);
+    public static void main(String[] args) {
+        InventoryService inventoryService = new InventoryService(new InventoryDao());
+        try {
+            // add items
+            inventoryService.addItem("Amul", "Milk", 100);
+            inventoryService.addItem("Amul", "Curd", 50);
+            inventoryService.addItem("Nestle", "Milk", 60);
+            inventoryService.addItem("Nestle", "Curd", 90);
 
-			inventoryService.addInventory("Amul", "Milk", 20);
-			inventoryService.addInventory("Amul", "Curd", 5);
-			inventoryService.addInventory("Nestle", "Milk", 5);
-			inventoryService.addInventory("Nestle", "Curd", 10);
-			inventoryService.addInventory("Amul", "Milk", 7);
+            inventoryService.addInventory("Amul", "Milk", 20);
+            inventoryService.addInventory("Amul", "Curd", 5);
+            inventoryService.addInventory("Nestle", "Milk", 5);
+            inventoryService.addInventory("Nestle", "Curd", 10);
+            inventoryService.addInventory("Amul", "Milk", 7);
 
-			//searchItem(brand, category,priceFrom, priceTo, sortBy, orderBy)
-			System.out.println("Search by brand (Nestle):");
-			inventoryService.searchItem(Arrays.asList("Nestle"), null, null, null, "price", "asc");
+            //searchItem(brand, category,priceFrom, priceTo, sortBy, orderBy)
+            System.out.println("Search by brand (Nestle):");
+            inventoryService.searchItem(Arrays.asList("Nestle"), null, null, null, "price", "asc")
+                    .forEach(System.out::println);
 
-			System.out.println("\nSearch by category (Milk):");
-			inventoryService.searchItem(null, Arrays.asList("Milk"), null, null, "price", "asc")
-					.forEach(System.out::println);
+            System.out.println("\nSearch by category (Milk):");
+            inventoryService.searchItem(null, Arrays.asList("Milk"), null, null, "price", "asc")
+                    .forEach(System.out::println);
 
-			System.out.println("\nSearch by category (Milk) ordered by price descending:");
-			inventoryService.searchItem(null, Arrays.asList("Milk"), null, null, "price", "desc")
-					.forEach(System.out::println);
+            System.out.println("\nSearch by category (Milk) ordered by price descending:");
+            inventoryService.searchItem(null, Arrays.asList("Milk"), null, null, "price", "desc")
+                    .forEach(System.out::println);
 
-			System.out.println("\nSearch by price range (70 to 100):");
-			inventoryService.searchItem(Arrays.asList("Nestle"), null, 70, 100, "price", "desc").forEach(System.out::println);
+            System.out.println("\nSearch by brand(Nestle) and price range (70 to 100):");
+            inventoryService.searchItem(Arrays.asList("Nestle"), null, 70, 100, "quantity", "desc").
+                    forEach(System.out::println);
 
-		} catch (Exception e) {
-			System.out.println("Error: " + e.getMessage());
-		}
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
 
-	}
+    }
 
 }
