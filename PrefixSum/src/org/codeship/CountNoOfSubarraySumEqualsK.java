d16275989a730e1420797dd879ecb7c2d045600f
RohanDhakad <dhakadrohan98>
2025-03-23 23:52:17 +0530

    No. of subarray sum equals k and contiguous array problems are solved & it is based on Prefix Sum Pattern

---
+++ PrefixSum/src/org/codeship/CountNoOfSubarraySumEqualsK.java
@@ -0,0 +1,31 @@
+package org.codeship;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountNoOfSubarraySumEqualsK {
+	
+	// TC: O(n * logn) in worst case
+	// SC: O(n)
+	public int subarraySum(int[] nums, int k) {
+		int n = nums.length;
+		int count = 0;
+		int sum = 0;
+		Map<Integer, Integer> hmap = new HashMap<>();
+		hmap.put(0, 1);
+		for (int i = 0; i < n; i++) {
+			sum += nums[i];
+			int remain = sum - k;
+			if (hmap.containsKey(remain)) {
+				count += hmap.get(remain);
+			}
+			hmap.put(sum, hmap.getOrDefault(sum, 0) + 1);
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
